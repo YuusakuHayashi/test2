c8e5697579d8c4dbcef110af82c5184bde9507e8
yuusaku.hayashi <yuusaku.hayashi@outlook.jp>
2020-07-25 15:26:32 +0900

    test

--- test2/Model.vb
+++ test2/Model.vb
@@ -1,32 +1,49 @@
 ﻿Imports System.Collections.ObjectModel
+Imports Newtonsoft.Json
 
-'Public Class Model
-'    Public Name As String
-'    Public Server As ServerModel
-'End Class
+Delegate Sub mySub()
 
 Public Class ServerModel
-    Public ServerName As String
-End Class
+    Private _SaveName As String
+    Public Property SaveName As String
+        Get
+            Return _SaveName
+        End Get
+        Set(value As String)
+            _SaveName = value
+        End Set
+    End Property
 
-Public Class DataBase
-    Public DataBaseName As String
-    Public DataBaseChild As List(Of DataBase)
-End Class
+    Private _FieldName As String
+    Public Property FieldName As String
+        Get
+            Return _FieldName
+        End Get
+        Set(value As String)
+            _FieldName = value
+        End Set
+    End Property
 
-Public Class DataTable
-    Public DataTableName As String
-    Public DataTableChild As List(Of DataTable)
-End Class
 
-Public Class Person
-    Public Name As String
-    Public Child As ObservableCollection(Of Person)
-End Class
+    Private _SourceValue As String
+    Public Property SourceValue As String
+        Get
+            Return _SourceValue
+        End Get
+        Set(value As String)
+            _SourceValue = value
+        End Set
+    End Property
 
-Public Class Model
-    'Private _Name As String
-    'Private _Functions As List(Of Plugin)
+    Private _DistinationValue As String
+    Public Property DistinationValue As String
+        Get
+            Return _DistinationValue
+        End Get
+        Set(value As String)
+            _DistinationValue = value
+        End Set
+    End Property
 
     Private _ServerName As String
     Public Property ServerName As String
@@ -57,14 +74,190 @@ Public Class Model
             _DataTableName = value
         End Set
     End Property
+    Private Property _RealName As String
+    Public Property RealName As String
+        Get
+            Return _RealName
+        End Get
+        Set(value As String)
+            _RealName = value
+        End Set
+    End Property
 
-    Private _Child As List(Of Model)
-    Public Property Child As List(Of Model)
+    Private _Child As List(Of ServerModel)
+    Public Property Child As List(Of ServerModel)
         Get
             Return _Child
         End Get
-        Set
-            _Child = Value
+        Set(value As List(Of ServerModel))
+            _Child = value
         End Set
+    End Property
+End Class
+
+Public Class Model
+    Private Const SHIFT_JIS = "SHIFT_JIS"
+
+    Private _Servers As List(Of ServerModel)
+    Public Property Servers As List(Of ServerModel)
+        Get
+            Return _Servers
+        End Get
+        Set(value As List(Of ServerModel))
+            _Servers = value
+        End Set
+    End Property
+
+    Public Function ServersLoad() As List(Of ServerModel)
+        ServersLoad = ModelLoad().Servers
+    End Function
+
+    Private ReadOnly Property ConfigFile As String
+        Get
+            Return MyDirectory & "\ConfigFile.json"
+        End Get
+    End Property
+
+    Private ReadOnly Property IniFile As String
+        Get
+            Return MyDirectory & "\IniFile"
+        End Get
+    End Property
+
+    Private ReadOnly Property MyDirectory As String
+        Get
+            Return System.Environment.GetEnvironmentVariable("USERPROFILE") & "\test"
+        End Get
+    End Property
+
+    Private Sub MyDirectoryEstablish()
+        System.IO.Directory.CreateDirectory(Me.MyDirectory)
+    End Sub
+
+    Private Sub IniFileEstablish()
+        System.IO.File.Create(Me.IniFile)
+    End Sub
+
+    Private Sub ConfigFileEstablish()
+        System.IO.File.Create(Me.ConfigFile)
+    End Sub
+
+    Private Function ModelLoad() As Model
+        Dim txt As String
+        Dim sr As System.IO.StreamReader
+        Dim res As Int32
+
+        ModelLoad = Nothing
+
+        'いずれの例外も初期化
+        Try
+            res = _MyDirectoryState
+            Select Case res
+                Case 0
+                    sr = New System.IO.StreamReader(
+                        Me.ConfigFile, System.Text.Encoding.GetEncoding(SHIFT_JIS)
+                    )
+                    txt = sr.ReadToEnd()
+                    ModelLoad = JsonConvert.DeserializeObject(Of Model)(txt)
+                Case 1, 99, 999
+                    Throw New Exception
+                Case Else
+                    Throw New Exception
+            End Select
+
+        Catch ex As Exception
+            '
+        Finally
+            If sr IsNot Nothing Then
+                sr.Close()
+                sr = Nothing
+            End If
+        End Try
+    End Function
+
+    Public Sub ModelSave()
+        Dim txt As String
+        Dim sw As System.IO.StreamWriter
+
+        Dim ms As mySub
+        ms = Sub()
+                 txt = JsonConvert.SerializeObject(Me)
+                 sw = New System.IO.StreamWriter(
+                    Me.ConfigFile, False, System.Text.Encoding.GetEncoding(SHIFT_JIS)
+                 )
+                 sw.WriteLine(txt)
+             End Sub
+        Dim ms2 As mySub : ms2 = AddressOf MyDirectoryEstablish
+        Dim ms3 As mySub : ms3 = AddressOf IniFileEstablish
+
+        Dim res As Int32 : res = _MyDirectoryState
+        Try
+            Select Case res
+                Case 0, 1
+                    Call ms()
+                Case 99
+                    '
+                Case 999
+                    Call ms2()
+                    Call ms3()
+                    Call ms()
+                Case Else
+                    '
+            End Select
+        Catch ex As Exception
+            '
+        Finally
+            If sw IsNot Nothing Then
+                sw.Close()
+                sw.Dispose()
+            End If
+        End Try
+    End Sub
+
+    Private ReadOnly Property _MyDirectoryState As Int32
+        Get
+            If _DirectoryCheck(Me.MyDirectory) Then
+                If _FileCheck(Me.IniFile) Then
+                    If _FileCheck(Me.ConfigFile) Then
+                        Return 0    '正常
+                    Else
+                        Return 1    'Configファイルが存在せず、初回に発生しうる
+                    End If
+                Else
+                    Return 99       '既に同名ファイルが存在
+                End If
+            Else
+                Return 999          'ディレクトリが存在しない
+            End If
+        End Get
+    End Property
+
+
+    Private ReadOnly Property _DirectoryCheck(d) As Boolean
+        Get
+            Try
+                If System.IO.Directory.Exists(d) Then
+                    Return True
+                Else
+                    Return False
+                End If
+            Catch ex As Exception
+                Throw ex
+            End Try
+        End Get
+    End Property
+
+    Private ReadOnly Property _FileCheck(ByVal f As String) As Boolean
+        Get
+            Try
+                If System.IO.File.Exists(f) Then
+                    Return True
+                Else
+                    Return False
+                End If
+            Catch ex As Exception
+                Throw ex
+            End Try
+        End Get
     End Property
 End Class
--- test2/ViewModel.vb
+++ test2/ViewModel.vb
@@ -1,65 +1,238 @@
 ﻿Imports System.ComponentModel
 Imports System.Collections.ObjectModel
 
+Delegate Sub MySub()
+
 Public Class ViewModel : Implements INotifyPropertyChanged
 
     Public Event PropertyChanged As PropertyChangedEventHandler _
         Implements INotifyPropertyChanged.PropertyChanged
 
     Protected Sub RaisePropertyChanged(ByVal propertyName As String)
-        RaiseEvent PropertyChanged(Me,
-                                    New PropertyChangedEventArgs(propertyName))
+        RaiseEvent PropertyChanged(
+            Me, New PropertyChangedEventArgs(propertyName)
+        )
     End Sub
 
-
-    'Private _Name As String
-    'Public Property Name As String
-    '    Get
-    '        Return _Name
-    '    End Get
-    '    Set(value As String)
-    '        _Name = value
-    '    End Set
-    'End Property
-
-
-    'Private _PluginEntries As List(Of Plugin)
-    'Public Property PluginEntries As List(Of Plugin)
-    '    Get
-    '        Return _PluginEntries
-    '    End Get
-    '    Set(value As List(Of Plugin))
-    '        _PluginEntries = value
-    '    End Set
-    'End Property
-
-    Private _vm As List(Of Model)
-
-    Public Property vm As List(Of Model)
+    Private _ServerVM As List(Of ServerModel)
+    Public Property ServerVM As List(Of ServerModel)
         Get
-            Return _vm
+            Return _ServerVM
         End Get
-        Set(value As List(Of Model))
-            _vm = value
+        Set(value As List(Of ServerModel))
+            _ServerVM = value
         End Set
     End Property
 
-    Sub New()
-        Me.vm = New List(Of Model)
-        Me.vm.Add(New Model With {
-            .ServerName = "AServer",
-            .Child = New List(Of Model) From {
-                New Model With {.ServerName = "BServer", .Child = New List(Of Model) From {
-                    New Model With {.ServerName = "CServer", .Child = New List(Of Model) From {
-                        New Model With {.ServerName = "DServer"}
-                    }}
-                }},
-                New Model With {.ServerName = "B2Server", .Child = New List(Of Model) From {
-                    New Model With {.ServerName = "C2Server", .Child = New List(Of Model) From {
-                        New Model With {.ServerName = "D2Server"}
-                    }}
-                }}
-            }
-        })
+    Private __VM As Model
+    Private Property _VM As Model
+        Get
+            Return __VM
+        End Get
+        Set(value As Model)
+            __VM = value
+        End Set
+    End Property
+
+    Private _ServerName As String
+    Public Property ServerName As String
+        Get
+            Return _ServerName
+        End Get
+        Set(value As String)
+            _ServerName = value
+        End Set
+    End Property
+
+    Private Property _DataBaseName As String
+    Public Property DataBaseName As String
+        Get
+            Return _DataBaseName
+        End Get
+        Set(value As String)
+            _DataBaseName = value
+        End Set
+    End Property
+
+    Private Property _DataTableName As String
+    Public Property DataTableName As String
+        Get
+            Return _DataTableName
+        End Get
+        Set(value As String)
+            _DataTableName = value
+        End Set
+    End Property
+
+    Private Property _FieldName As String
+    Public Property FieldName As String
+        Get
+            Return _FieldName
+        End Get
+        Set(value As String)
+            _FieldName = value
+        End Set
+    End Property
+
+    Private Property _SourceValue As String
+    Public Property SourceValue As String
+        Get
+            Return _SourceValue
+        End Get
+        Set(value As String)
+            _SourceValue = value
+        End Set
+    End Property
+
+    Private Property _DistinationValue As String
+    Public Property DistinationValue As String
+        Get
+            Return _DistinationValue
+        End Get
+        Set(value As String)
+            _DistinationValue = value
+        End Set
+    End Property
+
+
+    Private _IniFile As String
+    Public Property IniFile As String
+        Get
+            Return _IniFile
+        End Get
+        Set(value As String)
+            _IniFile = value
+        End Set
+    End Property
+
+    Private _ConfigFile As String
+    Public Property ConfigFile As String
+        Get
+            Return _ConfigFile
+        End Get
+        Set(value As String)
+            _ConfigFile = value
+        End Set
+    End Property
+
+    Private _MyDirectory As String
+    Public Property MyDirectory As String
+        Get
+            Return _MyDirectory
+        End Get
+        Set(value As String)
+            _MyDirectory = value
+        End Set
+    End Property
+
+    Private Property _ServerSelection As List(Of ServerModel)
+    Public Property ServerSelection As List(Of ServerModel)
+        Get
+            Return _ServerSelection
+        End Get
+        Set(value As List(Of ServerModel))
+            _ServerSelection = value
+        End Set
+    End Property
+
+    Private Property _DataBaseSelection As List(Of String)
+    Public Property DataBaseSelection As List(Of String)
+        Get
+            Return _DataBaseSelection
+        End Get
+        Set(value As List(Of String))
+            _DataBaseSelection = value
+        End Set
+    End Property
+
+    Private Property _DataTableSelection As List(Of String)
+    Public Property DataTableSelection As List(Of String)
+        Get
+            Return _DataTableSelection
+        End Get
+        Set(value As List(Of String))
+            _DataTableSelection = value
+        End Set
+    End Property
+
+    Private Sub SaveCommandExecute(ByVal p As Object)
     End Sub
+
+    Private _SaveCommand As ICommand
+    Public ReadOnly Property SaveCommand As ICommand
+        Get
+            If _SaveCommand Is Nothing Then
+                _SaveCommand = New DelegateCommand With {
+                    .ExecuteDelegater = AddressOf ,
+                    .CanExecuteDelegater = AddressOf
+                }
+            End If
+        End Get
+    End Property
+
+
+    Sub New()
+        Me._VM = New Model
+        Me.ServerVM = New List(Of ServerModel)
+
+        Me._VM.Servers = Me._VM.ServersLoad()
+
+        Me.ServerVM = Me._VM.Servers
+        Me.ServerName = Me._VM.Servers(0).ServerName
+        Me.DataBaseName = Me._VM.Servers(0).DataBaseName
+        Me.DataTableName = Me._VM.Servers(0).DataTableName
+        'Me._VM.Servers = New List(Of ServerModel) From {
+        '    New ServerModel With {
+        '        .RealName = "AAA", .Child = New List(Of ServerModel) From {
+        '            New ServerModel With {
+        '                .RealName = "ABA"
+        '            },
+        '            New ServerModel With {
+        '                .RealName = "ACA"
+        '            }
+        '        }
+        '    },
+        '    New ServerModel With {
+        '        .RealName = "BAA", .Child = New List(Of ServerModel) From {
+        '            New ServerModel With {
+        '                .RealName = "BBA"
+        '            },
+        '            New ServerModel With {
+        '                .RealName = "BCA"
+        '            }
+        '        }
+        '    }
+        '}
+        'Me.ServerVM = Me._VM.Servers
+        'Me._VM.ModelSave()
+        'Dim s As ServerModel
+        'Me._VM = New Model
+        's = Me._VM.ServerLoad
+        'Me.ServerVM = New List(Of ServerModel)
+        'Me.ServerVM.Add(s)
+    End Sub
+End Class
+
+Public Class DelegateCommand : Implements ICommand
+
+    Public Property CanExecuteDelegater As Func(Of Object, Boolean)
+    Public Property ExecuteDelegater As Action(Of String)
+
+    Public Event CanExecuteChanged As EventHandler Implements ICommand.CanExecuteChanged
+
+    Public Sub RaiseCanExecuteChanged()
+        RaiseEvent CanExecuteChanged(Me, Nothing)
+    End Sub
+
+    Public Sub Execute(parameter As Object) Implements ICommand.Execute
+        Dim d = ExecuteDelegater
+        If d <> Nothing Then
+            d(parameter)
+        End If
+    End Sub
+
+    Public Function CanExecute(ByVal parameter As Object) As Boolean Implements ICommand.CanExecute
+        Dim d = CanExecuteDelegater
+        Return IIf(d = Nothing, True, d(parameter))
+    End Function
 End Class
