a144a829ae1e0ef8dfcfcd1082f1d4a4d82d5aa5
YuusakuHayashi <YuusakuHayashi@outlook.jp>
2020-09-01 07:26:24 +0900

    test

---
+++ test2/ConditionModel.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Collections.ObjectModel
+
+Public Class ConditionModel
+    Private _FieldName As String
+    Public Property FieldName As String
+        Get
+            Return Me._FieldName
+        End Get
+        Set(value As String)
+            Me._FieldName = value
+        End Set
+    End Property
+
+    Private _FieldValue As Object
+    Public Property FieldValue As Object
+        Get
+            Return Me._FieldValue
+        End Get
+        Set(value As Object)
+            Me._FieldValue = value
+        End Set
+    End Property
+
+    Private _Conditions As ObservableCollection(Of ConditionModel)
+    Public Property Conditions As ObservableCollection(Of ConditionModel)
+        Get
+            Return Me._Conditions
+        End Get
+        Set(value As ObservableCollection(Of ConditionModel))
+            Me._Conditions = value
+        End Set
+    End Property
+End Class
---
+++ test2/ImigraterViewModel.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Collections.ObjectModel
+
+Public Class ImigraterViewModel
+    Private _Conditions As ObservableCollection(Of ConditionModel)
+    Public Property Conditions As ObservableCollection(Of ConditionModel)
+        Get
+            Return Me._Conditions
+        End Get
+        Set(value As ObservableCollection(Of ConditionModel))
+            Me._Conditions = value
+        End Set
+    End Property
+End Class
