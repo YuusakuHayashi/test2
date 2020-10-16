d0ea5fecc44987c29f54f4c6f18ff3f75756a163
U-MBC\yuusaku.hayashi <yuusaku.hayashi@PC470S.mbc.local>
2020-10-16 21:04:38 +0900

    test

--- test2/DBTestModel.vb
+++ test2/DBTestModel.vb
@@ -1,5 +1,17 @@
-﻿Public Class DBTestModel : Inherits BaseModel(Of DBTestModel)
+﻿Imports System.Collections.ObjectModel
+
+Public Class DBTestModel : Inherits BaseModel(Of DBTestModel)
     Public ServerName As String
     Public DataBaseName As String
     Public ConnectionString As String
+    Public Server As ServerModel
+    Sub New()
+        Server = New ServerModel With {
+            .DataBases = New ObservableCollection(Of DataBaseModel) From {
+                New DataBaseModel With {.Name = "hoge"},
+                New DataBaseModel With {.Name = "fuga"},
+                New DataBaseModel With {.Name = "piyo"}
+            }
+        }
+    End Sub
 End Class
