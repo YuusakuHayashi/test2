1ccc32f4cce4f6241a5e695593c5502060e64947
YuusakuHayashi <YuusakuHayashi@outlook.jp>
2020-11-27 00:59:09 +0900

    test

--- test2/Converters/RotateAngleConverter.vb
+++ test2/Converters/RotateAngleConverter.vb
@@ -4,19 +4,7 @@ Imports System.Drawing
 Public Class RotateAngleConverter : Implements IValueConverter
 
     Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.Convert
-        Dim s = CType(value, Single)
-        Select Case value
-            Case "Bottom"
-                Return Dock.Bottom
-            Case "Left"
-                Return Dock.Left
-            Case "Right"
-                Return Dock.Right
-            Case "Top"
-                Return Dock.Top
-            Case Else
-                Return Dock.Top
-        End Select
+        Return New Media.RotateTransform(value)
     End Function
 
     Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
