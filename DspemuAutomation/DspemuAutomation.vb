b05a995f0159b5f60c545871cb35bba4fe56d385
U-MBC\yuusaku.hayashi <yuusaku.hayashi@PC470S.mbc.local>
2021-02-19 21:38:55 +0900

    test

---
+++ DspemuAutomation/DspemuAutomation.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Windows.Forms
+Imports System.IO
+
+Module DspemuAutomation
+    Sub Main()
+        Dim exef As String = Application.ExecutablePath
+        Dim rpaf As String = $"{Path.GetDirectoryName(exef)}\dspemuautomation.json"
+        Dim errf As String = $"{Path.GetDirectoryName(exef)}\error"
+        Dim chkf As String = $"{Path.GetDirectoryName(exef)}\check"
+        Dim rpa As Rpa11.Rpa11 = Rpa11.Rpa11.LoadFromFile(rpaf)
+
+        If File.Exists(errf) Then
+            File.Delete(errf)
+        End If
+        If File.Exists(chkf) Then
+            File.Delete(chkf)
+        End If
+
+        If rpa IsNot Nothing Then
+            If rpa.CheckRun() Then
+                rpa.Run()
+            Else
+                File.Create(chkf)
+            End If
+        Else
+            File.Create(errf)
+        End If
+    End Sub
+End Module
